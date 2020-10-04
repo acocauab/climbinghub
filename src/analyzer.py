83b895832bd782566a2ea88bcb19071497679a76
AlbertCoca <albert.coca@mitigasolutions.com>
2020-10-04 20:32:04 +0200

    Added analyzer method description

--- src/analyzer.py
+++ src/analyzer.py
@@ -7,6 +7,25 @@ Short description
 
 # ==== CONSTANTS DEFINITIONS ==================================================
 
+
 # ==== CLASS DEFINITION =======================================================
 class Analyzer():
-    pass
+    def __init__(self, website: str, webpage: str) -> None:
+        self.webpage = webpage
+        # TODO: load grades
+
+    def _parse_grades(self) -> None:
+        pass
+
+    def _parse_tags(self) -> None:
+        pass
+
+    def _parse_name(self) -> None:
+        pass
+
+    def _parse_photos(self) -> None:
+        pass
+
+    def _parse_gps(self) -> None:
+        # TODO: To be done in the future.
+        pass
